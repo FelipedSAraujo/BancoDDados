9d0941a78c285e7a7afe9f2ac64ba09ef36103ad
Felipe de S. Araujo <felipearaujoptc@gmail.com>
2024-12-09 18:55:54 -0300

    Iniciando projeto

--- PrimeiraInteraçãoCBancoDDados.py
+++ PrimeiraInteraçãoCBancoDDados.py
@@ -1,1 +1,14 @@
-print("hello word")
+print("Hello word, oque voce precisa?")
+print("1-Cadastrar uma conta")
+print("2-Entrar em uma conta")
+Querer = int(input(""))
+
+def escolha (numero):
+    print("Você escolheu a opção {numero}")
+
+if Querer == 1:
+    escolha(Querer)
+elif Querer == 2 :
+    escolha(Querer)
+else: 
+    print("Por favor, insira um valor entre 1 e 2")
